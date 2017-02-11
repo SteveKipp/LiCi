e92c304e436079d6a600fb43fca3844e64cd33bc
Steve Kipp <sakqm2@mst.edu>
2017-02-11 12:37:25 -0600

    remote class pulls if the source directory alread exists and clones if it does not

--- LiCi/commands/options/remote.py
+++ LiCi/commands/options/remote.py
@@ -8,10 +8,24 @@ class Remote(object):
         if repo[-3:] == "git":
             print("git repo detected -> using git")
             #does this already exist? -> no:clone -> yes:pull
-            git_clone =subprocess.Popen(["git", "clone", repo])
-            git_clone.wait()
+            name = self.get_repo_folder_git(repo)
+            if os.path.exists(os.getcwd() + "/{}".format(name)):
+                #pull
+                os.chdir(os.getcwd() + "/{}".format(name))
+                git_pull = subprocess.Popen(["git", "pull"])
+                git_pull.wait()
+            else:
+                #clone
+                git_clone = subprocess.Popen(["git", "clone", repo])
+                git_clone.wait()
         if repo[:3] == "svn":
             print("svn repo detected -> using subversion")
+            #soon
 
         self.repo = repo
         self.location = "~/.LiCi/routines"
+
+    def get_repo_folder_git(self, repo_url):
+        folders = repo_url.split("/")
+        folder_name = folders[len(folders)-1][:-4]
+        return folder_name
